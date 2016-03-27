906954da89195bcf024ff9edf88286ddc33f64e3
Tsvetelin Kulinski <tsvetelin.kulinski@gmail.com>
2016-03-27 15:51:27 +0300

    introduce parse

--- www/js/app.js
+++ www/js/app.js
@@ -3,71 +3,127 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
+angular.module('starter',
+    [
+        'ionic',
+        'app.controllers',
+        'app.services',
+        'user.controllers',
+        'user.services'
+    ]
+)
+/**
+ * see documentation: https://www.parse.com/apps/quickstart#parse_data/web/existing
+ *
+ * SET THESE VALUES IF YOU WANT TO USE PARSE, COMMENT THEM OUT TO USE THE DEFAULT
+ * SERVICE
+ *
+ * parse constants
+ */
+    .value('ParseConfiguration', {
+        applicationId: "asdadaswetwetghhdghd",
+        javascriptKey: "sajdhalsfaldkfoadf",
+        server: "http://localhost:1337/parse"
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+/**
+ *
+ */
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+            // create account state
+            .state('app-signup', {
+                url: "/signup",
+                templateUrl: "templates/user/signup.html",
+                controller: "SignUpController"
+            })
+            // login state that is needed to log the user in after logout
+            // or if there is no user object available
+            .state('app-login', {
+                url: "/login",
+                templateUrl: "templates/user/login.html",
+                controller: "LoginController"
+            })
+
+            // setup an abstract state for the tabs directive, check for a user
+            // object here is the resolve, if there is no user then redirect the
+            // user back to login state on the changeStateError
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html",
+                resolve: {
+                    user: function (UserService) {
+                        var value = UserService.init();
+                        return value;
+                    }
+                }
+            })
+
+            // Each tab has its own nav history stack:
+            .state('tab.list', {
+                url: '/list',
+                views: {
+                    'tab-list': {
+                        templateUrl: 'templates/tab-list.html',
+                        controller: 'ListCtrl'
+                    }
+                }
+            })
+            .state('tab.list-detail', {
+                url: '/list/:itemId',
+                views: {
+                    'tab-list': {
+                        templateUrl: 'templates/list-detail.html',
+                        controller: 'ListDetailCtrl'
+                    }
+                }
+            })
+
+            .state('tab.account', {
+                url: '/account',
+                cache: false,
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/list');
+
+    })
+    .run(function ($ionicPlatform, $rootScope, $state) {
+
+
+        $rootScope.$on('$stateChangeError',
+            function (event, toState, toParams, fromState, fromParams, error) {
+
+                debugger;
+
+                console.log('$stateChangeError ' + error && (error.debug || error.message || error));
+
+                // if the error is "noUser" the go to login state
+                if (error && error.error === "noUser") {
+                    event.preventDefault();
+
+                    $state.go('app-login', {});
+                }
+            });
+
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
