82ba572de5074e39d5fc853ec3ab06f785bb8e59
Tsvetelin Kulinski <tsvetelin.kulinski@gmail.com>
2016-03-28 22:04:08 +0300

    parse login in service

--- www/js/travel/controllers.js
+++ www/js/travel/controllers.js
@@ -5,40 +5,31 @@
  */
 angular.module('travel.controllers', [])
   .controller('TravelListCtrl', [
-      '$state', '$scope', '$stateParams', 'UserService', 'TravelService', // <-- controller dependencies
-      function($state, $scope, $stateParams, UserService, TravelService) {
-        TravelService.findAllTravels().then(function(travels) {
-          console.log(travels.length);
-          $scope.travelList = travels;
-        })
-      }
-])
-.controller('TravelCreateCtrl', [
-  '$state', '$scope', '$stateParams', 'UserService', 'TravelService', // <-- controller dependencies
-  function($state, $scope, $stateParams, UserService) {
-    $scope.travel = {
-      from: "Lovech",
-      to: "Sofia",
-      seats: 4
-    };
-    $scope.createTravel = function() {
-      UserService.currentUser().then(function(_user) {
-        var Travel = Parse.Object.extend('Travel');
-        var myTravel = new Travel();
-        myTravel.set('from', $scope.travel.from);
-        myTravel.set('to', $scope.travel.to);
-        myTravel.set('seats', parseInt($scope.travel.seats));
-        myTravel.set('parent', _user);
-        myTravel.save(null, {
-          success: function(travel) {
-            alert("Your travel was created!Travel Id: " + travel.id);
+    '$state', '$scope', '$stateParams', 'UserService', 'TravelService', // <-- controller dependencies
+    function($state, $scope, $stateParams, UserService, TravelService) {
+      TravelService.findAllTravels().then(function(travels) {
+        console.log(travels.length);
+        $scope.travelList = travels;
+      })
+    }
+  ])
+  .controller('TravelCreateCtrl', [
+    '$state', '$scope', '$stateParams', 'UserService', 'TravelService', // <-- controller dependencies
+    function($state, $scope, $stateParams, UserService, TravelService) {
+      $scope.travel = {
+        from: "Lovech",
+        to: "Sofia",
+        seats: 4
+      };
+      $scope.createTravel = function() {
+        UserService.currentUser()
+          .then(function(_user) {
+            return TravelService.createTravel(_user, $scope.travel)
+          })
+          .then(function(travel) {
+            console.log("Successfully created travel: " + JSON.stringify(travel))
             $state.go('travel.list');
-          },
-          error: function(travel, error) {
-            alert('Failed to create new travel, with error code: ' + error.message);
-          }
-        });
-      });
-    };
-  }
-]);
+          });
+      };
+    }
+  ]);
--- www/js/travel/services.js
+++ www/js/travel/services.js
@@ -1,37 +1,56 @@
 angular.module('travel.services', [])
 
 .service('TravelService', ['$q', 'ParseConfiguration',
-  function($q, ParseConfiguration) {
-    return {
-      findMyTravels: function(_user) {
-        var defered = $q.defer();
-        var Travel = Parse.Object.extend('Travel');
-        var travel = new Parse.Query(Travel);
-        travel.equalTo("parent", _user);
-        travel.find({
-          success: function(travels) {
-            defered.resolve(travels);
-          },
-          error: function(err) {
-            defered.reject(travels);
-          }
-        });
-        return defered.promise;
-      },
-      findAllTravels: function() {
-        var defered = $q.defer();
-        var Travel = Parse.Object.extend('Travel');
-        var travel = new Parse.Query(Travel);
-        travel.find({
-          success: function(travels) {
-            defered.resolve(travels);
-          },
-          error: function(err) {
-            defered.reject(travels);
-          }
-        });
-        return defered.promise;
-      }
+function($q, ParseConfiguration) {
+  return {
+    findMyTravels: function(_user) {
+      var defered = $q.defer();
+      var Travel = Parse.Object.extend('Travel');
+      var travel = new Parse.Query(Travel);
+      travel.equalTo("parent", _user);
+      travel.find({
+        success: function(travels) {
+          defered.resolve(travels);
+        },
+        error: function(err) {
+          defered.reject(travels);
+        }
+      });
+      return defered.promise;
+    },
+    findAllTravels: function() {
+      var defered = $q.defer();
+      var Travel = Parse.Object.extend('Travel');
+      var travel = new Parse.Query(Travel);
+      travel.find({
+        success: function(travels) {
+          defered.resolve(travels);
+        },
+        error: function(err) {
+          defered.reject(travels);
+        }
+      });
+      return defered.promise;
+    },
+    createTravel: function(_user, travelParams) {
+      var defered = $q.defer();
+      var Travel = Parse.Object.extend('Travel');
+      var myTravel = new Travel();
+      myTravel.set('from', travelParams.from);
+      myTravel.set('to', travelParams.to);
+      myTravel.set('seats', parseInt(travelParams.seats));
+      myTravel.set('parent', _user);
+      myTravel.save(null, {
+        success: function(travel) {
+          alert("Your travel was created!Travel Id: " + travel.id);
+          defered.resolve(travel);
+        },
+        error: function(travel, error) {
+          alert('Failed to create new travel, with error code: ' + error.message);
+          defered.reject(error);
+        }
+      });
+      return defered.promise;
     }
-  }
-]);
+}
+}]);
