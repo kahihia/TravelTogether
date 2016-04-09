57dad26863a4ae21cc15a5f16afbb1ad33ff3ff2
Tsvetelin Kulinski <tsvetelin.kulinski@gmail.com>
2016-04-09 08:38:23 +0300

    added details page and save profile of user

--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -5,12 +5,11 @@
  */
 angular.module('app.controllers', [])
   .controller('AccountCtrl', [
-    '$state', '$scope', 'UserService', // <-- controller dependencies
-    function($state, $scope, UserService) {
+    '$state', '$scope', 'UserService', 'AppService', // <-- controller dependencies
+    function($state, $scope, UserService, AppService) {
       UserService.currentUser().then(function(_user) {
         $scope.user = _user;
       });
-
       $scope.doLogoutAction = function() {
         UserService.logout().then(function() {
           $state.go('app-login');
@@ -18,5 +17,29 @@ angular.module('app.controllers', [])
           alert("error logging in " + _error.debug);
         })
       };
+      $scope.saveProfileDetails = function() {
+        UserService.currentUser()
+          .then(function(_user) {
+            return AppService.saveProfile($scope.profile, $scope.profileParams)
+          })
+          .then(function(profile) {
+            console.log("Successfully created profile: " + JSON.stringify(profile))
+          });
+      };
+      $scope.getProfileDetails = function() {
+        UserService.currentUser()
+          .then(function(_user) {
+            return AppService.getProfile(_user)
+          })
+          .then(function(profile) {
+            $scope.profile = profile;
+            $scope.profileParams = {
+              age: profile.get('age'),
+              gender: profile.get('gender'),
+              avatar: profile.get('avatar')
+            }
+          });
+      };
+      $scope.getProfileDetails();
     }
   ]);
--- www/js/app/services.js
+++ www/js/app/services.js
@@ -1,6 +1,57 @@
 angular.module('app.services', [])
+  .service('AppService', ['$q', 'ParseConfiguration',
+    function($q, ParseConfiguration) {
+      return {
+        /**
+         * params: profile - profile of current user, params - params from input
+         */
+        saveProfile: function(profile, params) {
+          var defered = $q.defer();
+          profile.set('age', params.age);
+          profile.set('gender', params.gender);
+          profile.set('avatar', params.avatar);
+          profile.save(null, {
+            success: function(profile) {
+              console.log("Profile: Saved " + JSON.stringify(profile));
+              defered.resolve(profile);
+            },
+            error: function(profile, error) {
+              alert('Failed to save profile ' + error.message);
+              defered.reject(error);
+            }
+          });
+          return defered.promise;
+        },
+        /**
+         * params: _user - parent of profile
+         * if no profile for user, create new one
+         */
+        getProfile: function(_user) {
+          var defered = $q.defer();
+          var Profile = Parse.Object.extend('Profile');
+          var profile = new Parse.Query(Profile);
+          profile.equalTo("parent", _user);
+          profile.find({
+            success: function(profile) {
+              if (profile.length == 0) {
+                console.log("Profile: Not found. Create New");
+                var p = new Profile();
+                p.set("parent", _user);
+                defered.resolve(p)
+              } else {
+                console.log("Profile: Found: " + JSON.stringify(profile[0]));
+                defered.resolve(profile[0])
+              }
+              defered.resolve(profile);
+            },
+            error: function(profile, err) {
+              console.log("Profile:  Error: " + JSON.stringify(err));
+              defered.reject(profile);
+            }
 
-    .service('AppService', ['$q', 'ParseConfiguration',
-        function ($q, ParseConfiguration) {
-
-        }]);
+          });
+          return defered.promise;
+        }
+      }
+    }
+  ]);
