0801c06961667eb236df5daa79cda41e4957f7a7
Tsvetelin Kulinski <tsvetelin.kulinski@gmail.com>
2016-04-30 07:04:53 +0300

    get messages implemented

--- www/js/app/services.js
+++ www/js/app/services.js
@@ -5,16 +5,33 @@ angular.module('app.services', [])
         getMessages: function(profileId1, profileId2){
           var defered = $q.defer();
           var Message = Parse.Object.extend('Message');
-          var messageQuery = new Parse.Query(Message);
-          defered.resolve([{},{}]);
+          var query1 = new Parse.Query(Message);
+          console.log(profileId1);
+          console.log(profileId2);
+          query1.equalTo("sender_profile_id", profileId1);
+          query1.equalTo("recipient_profile_id", profileId2);
+          var query2 = new Parse.Query(Message);
+          query2.equalTo("sender_profile_id", profileId2);
+          query2.equalTo("recipient_profile_id", profileId1);
+
+          var messageQuery = Parse.Query.or(query1, query2);
+          messageQuery.find({
+            success: function(messages) {
+              console.log(messages);
+              defered.resolve(messages);
+            },
+            error: function(err) {
+              defered.reject(err);
+            }
+          });
           return defered.promise;
         },
         sendMessage: function(fromProfile, toProfile, text) {
           var defered = $q.defer();
           var Message = Parse.Object.extend('Message');
           var m = new Message();
-          m.set('recipient_profile_id', fromProfile);
-          m.set('sender_profile_id', toProfile);
+          m.set('recipient_profile_id', toProfile);
+          m.set('sender_profile_id', fromProfile);
           m.set('text', text);
           m.save(null, {
             success: function(message) {
